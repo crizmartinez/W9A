843bd870c04d2ca834b056510155458402103864
cristabel <criztabel.martinez@gmail.com>
2020-11-13 22:52:29 -0700

    W9A: Variables and Conditionals

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var age = prompt("Type your age");
+var is_subscribed = confirm("are you subscribed?");
+
+
+// 4 scenarios
+// the user is younger than 18 and not subscribed
+if(age < 18 && is_subscribed == false) {
+    console.log(" the user is younger than 18 and not subscribed");
+}
+else if(age > 18 && is_subscribed == false) {
+    console.log(" the user is older than 18 and not subscribed");
+}
+// the user is younger than 18 and is subscribed
+else if(age < 18 && is_subscribed == true) {
+    console.log(" the user is younger than 18 and is subcribed");
+}
+
+else(age > 18 && is_subscribed == true) 
+    console.log(" the user is older than 18 and is subscribed")
